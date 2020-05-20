858d9f57ca83560a3280692b801ff850165c2a7e
FelipeMeganha <felipengmec@gmail.com>
2020-05-20 16:04:05 -0300

    Atualizacao site

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,17 +4,13 @@ import React from 'react'
 const Header = props => (
   <header id="header" style={props.timeout ? { display: 'none' } : {}}>
     <div className="logo">
-      <span className="icon fa-diamond"></span>
+      <span className="icon"></span>
     </div>
     <div className="content">
       <div className="inner">
-        <h1>Dimension</h1>
+        <h1>Felipe Meganha</h1>
         <p>
-          A fully responsive site template designed by{' '}
-          <a href="https://html5up.net">HTML5 UP</a> and released
-          <br />
-          for free under the{' '}
-          <a href="https://html5up.net/license">Creative Commons</a> license.
+        Data Scientist. Engenheiro de Inteligência Computacional. Engenheiro Mecatrônico. 
         </p>
       </div>
     </div>
